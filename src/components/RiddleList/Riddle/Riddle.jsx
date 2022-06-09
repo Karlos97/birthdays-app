3f1569cc6a7243c22a572f14c61a7669d8e44cd5
Karlos97 <k.minta97@gmail.com>
2022-06-09 03:26:18 -0700

    updating questions

--- src/components/RiddleList/Riddle/Riddle.jsx
+++ src/components/RiddleList/Riddle/Riddle.jsx
@@ -7,51 +7,64 @@ const Riddle = ({
   reward = 'a',
 }) => {
   const [userAnswer, setUserAnswer] = useState('');
-  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
-  const [firstRender, setFirstRender] = useState(true);
+  const [isAnswer, setIsAnswer] = useState({ label: '', value: false });
+  const [firstAnswer, setFirstAnswer] = useState(true);
+  const isPhraseCorrect = (phrase) => !!/^[a-zA-Z ]+$/.test(phrase);
 
   const checkAnswer = () => {
-    setFirstRender(false);
+    setFirstAnswer(false);
+    if (!isPhraseCorrect(userAnswer))
+      return setIsAnswer({ label: "Don't use Polish signs!", value: false });
+
     if (userAnswer.toLowerCase() === answer.toLowerCase()) {
-      setIsCorrectAnswer(true);
+      setIsAnswer({ label: 'CORRECT!', value: true });
     } else {
-      setIsCorrectAnswer(false);
+      setIsAnswer({ label: 'WRONG!', value: false });
     }
   };
 
   return (
-    <li className={classes.riddle}>
-      {!firstRender && (
-        <p
-          className={classes['riddle-top-subtitle']}
-          style={{ color: `${isCorrectAnswer ? 'green' : 'red'}` }}
-        >
-          {`${isCorrectAnswer ? 'CORRECT!' : 'WRONG!'}`}
-        </p>
-      )}
-      {isCorrectAnswer && (
-        <p
-          className={classes['riddle-top-subtitle']}
-          style={{ color: `${isCorrectAnswer ? 'green' : 'red'}` }}
-        >
-          {reward}
-        </p>
-      )}
-      <p className={classes['riddle-top-subtitle']}>{question}</p>
+    <div className={classes['riddle-wrapper']}>
+      <div
+        className={`${classes['button']} ${classes['button-answer']}`}
+        onClick={() =>
+          setIsAnswer({
+            label:
+              'Oj Ty kolego chyba chodziles bardziej na A a aaautomatyke niz robotyke :)',
+            value: true,
+          })
+        }
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Solution
+      </div>
+      <li className={classes.riddle}>
+        {(!firstAnswer || isAnswer.value) && (
+          <p
+            className={classes['riddle-top-subtitle']}
+            style={{ color: `${isAnswer.value ? 'green' : 'red'}` }}
+          >
+            {isAnswer?.label}
+          </p>
+        )}
+        {isAnswer?.value && (
+          <p
+            className={classes['riddle-top-subtitle']}
+            style={{ color: `${isAnswer?.value ? 'green' : 'red'}` }}
+          >
+            {reward}
+          </p>
+        )}
+        <p className={classes['riddle-top-subtitle']}>{question}</p>
 
-      <input
-        className={classes['input']}
-        onChange={(e) => setUserAnswer(e.target.value)}
-      />
+        <input
+          className={classes['input']}
+          onChange={(e) => setUserAnswer(e.target.value)}
+        />
 
-      <div>
-        <div className={classes['button']} onClick={setIsCorrectAnswer}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          Get answer now!
-        </div>
         <div className={classes['button']} onClick={() => checkAnswer()}>
           <span></span>
           <span></span>
@@ -59,8 +72,8 @@ const Riddle = ({
           <span></span>
           Check!
         </div>
-      </div>
-    </li>
+      </li>
+    </div>
   );
 };
 
--- src/components/RiddleList/RiddleList.jsx
+++ src/components/RiddleList/RiddleList.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { Fragment } from "react/cjs/react.production.min";
-import Riddle from "./Riddle/Riddle";
-import classes from "./RiddleList.module.scss";
+import React from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
+import Riddle from './Riddle/Riddle';
+import classes from './RiddleList.module.scss';
 
 const RiddleList = () => {
   return (
     <Fragment>
-      <ul className={classes["riddles-list"]}>
+      <ul className={classes['riddles-list']}>
         <Riddle
-          question="Co jest piekne i brzydkie?"
-          answer="Lama"
-          reward="53.01108, 18.60819"
+          question="Czy jesli Mariusz Pudzianowski bylby pierwszy w kulach to czy cos by to dalo?"
+          answer="to by nic nie dalo"
+          reward="(53.01108,"
         />
         <Riddle
-          question="Co jest piekne i brzydkie?"
-          answer="cegla"
-          reward="53.0081, 18.6040"
+          question="Jak sie nazywa uwielbiany przez studentow, najlepszy wykladowca politechniki Poznanskiej?"
+          answer="Warczynski"
+          reward=" 18.60819)"
+        />
+        <Riddle
+          question="Gdzie w ubieglych latach można bylo zjesc najlepszego kebsa w Poznaniu?"
+          answer="u dziadka"
+          reward="(53.0081,"
+        />
+        <Riddle
+          question="Gdzie jest tangensa alfa?"
+          answer="tu"
+          reward=" 18.6040)"
         />
       </ul>
     </Fragment>
